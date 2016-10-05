2385131693083eefda0a208e907e6f9d7132c074
Luis Gerardo Bravo <luis.g.bravo1@gmail.com>
2016-10-05 18:08:51 -0500

    Add files via upload
    Added Cases

--- 1381.cpp
+++ 1381.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <cstdio>
+#include <math.h>
 using namespace std;
 
 int main() {
     
-    int iDistance, iVelocity1, iVelocity2, iFly;
+    int iDistance, iVelocity1, iVelocity2, iFly, iCases;
     double dDistTraveled, dTime;
     
-    cin >> iDistance >> iVelocity1 >> iVelocity2 >> iFly;
+    do {
+        cin >> iCases;
+    } while (iCases > 100);
     
-
-    dTime = (double)iDistance / (iVelocity1 + iVelocity2);
-    dDistTraveled = dTime * iFly;
-
+    for (int count = 0; count < iCases; count++) {
+        cin >> iDistance >> iVelocity1 >> iVelocity2 >> iFly;
     
-    printf ("%.2f\n", dDistTraveled);
+        dTime = (double)iDistance / (iVelocity1 + iVelocity2);
+        dDistTraveled += dTime * iFly;
+    }
+    
+    
+    printf ("%.2f\n", ceil(dDistTraveled));
     
 }
-
